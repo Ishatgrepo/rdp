f48c4f15b8821890f5594d5582a846e92f321b43
Ishatgrepo <dmcomdot@gmail.com>
2024-12-24 15:32:31 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import subprocess
 import shutil
 
 CRD_SSH_Code = input("Google CRD SSH Code :")
-username = "user" #@param {type:"string"}
-password = "root" #@param {type:"string"}
+username = "admin" #@param {type:"string"}
+password = "a" #@param {type:"string"}
 os.system(f"useradd -m {username}")
 os.system(f"adduser {username} sudo")
 os.system(f"echo '{username}:{password}' | sudo chpasswd")
@@ -18,7 +18,6 @@ class CRDSetup:
         os.system("apt update")
         self.installCRD()
         self.installDesktopEnvironment()
-        self.changewall()
         self.installGoogleChrome()
         self.installTelegram()
         self.installQbit()
@@ -56,27 +55,10 @@ class CRDSetup:
         print("Telegram Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
     @staticmethod
-    def changewall():
-        os.system(f"curl -s -L -k -o xfce-verticals.png https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/CachedImage_1024_768_POS4.jpg")
-        current_directory = os.getcwd()
-        custom_wallpaper_path = os.path.join(current_directory, "xfce-verticals.png")
-        destination_path = '/usr/share/backgrounds/xfce/'
-        shutil.copy(custom_wallpaper_path, destination_path)
-        print("Wallpaper Changed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-   
-    @staticmethod
     def installQbit():
         subprocess.run(["sudo", "apt", "update"])
         subprocess.run(["sudo", "apt", "install", "-y", "qbittorrent"])
         print("Qbittorrent Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-    @staticmethod
-    def finish(user):
-        if Autostart:
-            os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
-            link = "www.youtube.com/@The_Disala"
-            colab_autostart = """[Desktop Entry]
-            print("Finalizing !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 Type=Application
 Name=Colab
@@ -94,20 +76,9 @@ X-GNOME-Autostart-enabled=true""".format(link)
         os.system(f"su - {user} -c '{command}'")
         os.system("service chrome-remote-desktop start")
         
-        print("..........................................................") 
-        print(".....Brought By The Disala................................") 
-        print("..........................................................") 
-        print("......#####...######...####....####...##.......####.......") 
-        print("......##..##....##....##......##..##..##......##..##......")  
-        print("......##..##....##.....####...######..##......######......") 
-        print("......##..##....##........##..##..##..##......##..##......") 
-        print("......#####...######...####...##..##..######..##..##......") 
-        print("..........................................................") 
-        print("..Youtube Video Tutorial - https://youtu.be/xqpCQCJXKxU ..") 
-        print("..........................................................") 
         print("Log in PIN : 123456") 
-        print("User Name : user") 
-        print("User Pass : root") 
+        print("User Name : admin") 
+        print("User Pass : a") 
         while True:
             pass
 
